8330b5f438797653c1de691f3a0ef614059d60d3
Christian Loosli <loosli.christian@gmail.com>
2020-05-23 11:21:47 -0600

    Update app name and meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tictactoe';
+export class AppComponent implements OnInit {
+
+  title = 'TicTacToe PWA Game';
+
+  constructor(
+    private meta: Meta
+  ) { }
+
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'keywords', content: 'HTML, CSS, JavaScript, Angular, TicTacToe, PWA' });
+  }
+
 }
