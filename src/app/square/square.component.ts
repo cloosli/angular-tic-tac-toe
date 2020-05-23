f2b78335fd551c3d7b779a6cd1f12c108cb15655
Christian Loosli <loosli.christian@gmail.com>
2020-05-23 10:03:05 -0600

    Much better looking game with nabular buttons

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,10 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>{{ value }}</button>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value=='X'">{{value}}</button>
+    <button nbButton hero status="info" *ngIf="value=='O'">{{value}}</button>
   `,
   styles: [
-    'button {height:100%;width: 100%;font-size: 3rem;}'
+    'button { width: 100%; height: 100%; font-size: 5em !important; }'
   ]
 })
 export class SquareComponent {
